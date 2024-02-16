ceddb3deda986d510abad83fc72fe07d9fef436c
Robert Higgins <rhiggins@dsny.nyc.gov>
2024-02-16 08:59:50 -0500

    client-side pagination and prettier plugin lint fixes for templates.

---
+++ app/controllers/jobs.js
@@ -0,0 +1,25 @@
+import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+
+export default class JobsController extends Controller {
+  @tracked
+  page = 1;
+
+  @tracked
+  ipp = 30;
+
+  get pages() {
+    return Math.ceil(this.model.length / this.ipp);
+  }
+
+  get jobs() {
+    const { page, ipp } = this;
+    return this.model.slice((page - 1) * ipp, (page - 1) * ipp + ipp);
+  }
+
+  @action
+  changePage(offset) {
+    this.page += offset;
+  }
+}
