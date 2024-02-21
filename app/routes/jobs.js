4edd4af8c41af8012da70cf7d83cf335c54a9378
Robert Higgins <robert.d.higgins@gmail.com>
2024-02-21 17:49:50 +0000

    custom refresh logic depending on qp changed

--- app/routes/jobs.js
+++ app/routes/jobs.js
@@ -6,24 +6,6 @@ import { action } from '@ember/object';
 export default class JobsRoute extends Route {
   @service store;
 
-  queryParams = {
-    q: {
-      refreshModel: true,
-    },
-    agency: {
-      refreshModel: true,
-    },
-    tclass: {
-      refreshModel: true,
-    },
-    title: {
-      refreshModel: true,
-    },
-    salary: {
-      refreshModel: true,
-    },
-  };
-
   async model(params) {
     await new Promise((resolve) => later(resolve, 500));
     if (params) {
@@ -36,22 +18,23 @@ export default class JobsRoute extends Route {
 
   filter(results, params) {
     if (params.agency) {
-      console.log(params.agency);
     }
     if (params.tclass) {
-      console.log(params.tclass);
     }
     if (params.title) {
-      console.log(params.title);
     }
     if (params.salary) {
-      console.log(params.salary);
     }
     return results;
   }
 
   @action
-  queryParamsDidChange() {
+  queryParamsDidChange(changed) {
     window.scrollTo(0, 0);
+    if (Object.keys(changed).length === 1 && changed.page) {
+      return true;
+    } else {
+      this.refresh();
+    }
   }
 }
