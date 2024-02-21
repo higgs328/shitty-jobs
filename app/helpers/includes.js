4ea4ee68e880e2e13fd55414c84111cb3540a5df
Robert Higgins <robert.d.higgins@gmail.com>
2024-02-21 13:45:46 +0000

    preserve checkbox checked state

--- app/helpers/includes.js
+++ app/helpers/includes.js
@@ -1,7 +1,5 @@
 import { helper } from '@ember/component/helper';
 
 export default helper(function includes(positional /*, named*/) {
-  const a = positional[0];
-  const b = positional[1];
-  return Boolean(a && b ? a.includes(b) : false);
+  return Boolean(positional[0].includes(positional[1]));
 });
