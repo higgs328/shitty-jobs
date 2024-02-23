4d1dfea72e4bb2444cdbe6674e18651456db1f2f
Robert Higgins <rhiggins@dsny.nyc.gov>
2024-02-23 13:44:24 -0500

    added favicon

--- app/helpers/search.js
+++ app/helpers/search.js
@@ -3,5 +3,7 @@ import { helper } from '@ember/component/helper';
 export default helper(function search(positional /*, named*/) {
   const a = positional[0];
   const b = positional[1];
-  return Boolean((!a || !b ) ? true : a.toUpperCase().search(b.toUpperCase()) > -1);
+  return Boolean(
+    !a || !b ? true : a.toUpperCase().search(b.toUpperCase()) > -1,
+  );
 });
